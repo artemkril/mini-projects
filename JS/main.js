89cdbcaa5c4d2a3b369232f044f9e32596024493
Artem Kril <artemkent9@gmail.com>
2023-12-17 23:54:59 +0200

    Function of creating li element

--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,49 @@
 const mainInput = document.getElementById("input_box");
 const mainList = document.getElementById("list_of_tasks");
 const buttInput = document.getElementById("addTask");
-function addTask(event){
-    event.preventDefault();
 
-    if (mainInput.value === ''){
+// Створення лішок
+function createTaskElement(taskText) {
+    let elementLi = document.createElement('li')
+    elementLi.innerHTML = taskText;
+
+    let span = document.createElement("span"); //створення хрестика (видалення)
+    span.innerHTML = '\u00d7';
+
+    elementLi.appendChild(span);
+    return elementLi;
+}
+
+// Додавання завдання
+function addTask(event) {
+    event.preventDefault();
+    if (mainInput.value === '') {
         alert('You must write something!');
     }
-    else{
-        let li = document.createElement('li');
-        li.innerHTML = mainInput.value;
-        mainList.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = '\u00d7';
-        li.appendChild(span);
+    else {
+        let newTask = createTaskElement(mainInput.value);
+        mainList.appendChild(newTask);
+        mainInput.value = '';
     }
-    mainInput.value = '';
+
     saveData();
 }
 buttInput.addEventListener("submit", addTask)
 
-mainList.addEventListener('click',  function(event){
-    if (event.target.tagName === "LI"){
+mainList.addEventListener('click', function (event) {
+    if (event.target.tagName === "LI") {
         event.target.classList.toggle("checked");
         saveData()
-    } else if(event.target.tagName === "SPAN"){
+    } else if (event.target.tagName === "SPAN") {
         event.target.parentElement.remove();
         saveData()
     }
-},false)
+}, false)
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", mainList.innerHTML)
 }
-function showList(){
+function showList() {
     mainList.innerHTML = localStorage.getItem("data")
 }
 showList()
